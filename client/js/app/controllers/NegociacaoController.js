8d998bfaf30b80ceaa0d2375c0a584067318f7d9
nilsonjrsilva <nilson.silva@fcamara.com.br>
2018-05-11 13:16:53 -0300

    Adicionando busca de dados do form com JQuery

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,17 @@
 class NegociacaoController {
-
     adicionar(event) {
+        // mudando o evento default do form
         event.preventDefault();
-        alert('Chamei a ação do controller');
+
+        // rodando no contexto do document
+        let $ = document.querySelector.bind(document);
+
+        let inputQuantidade = $('#quantidade');
+        let inputData = $('#data');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputValor.value);
+        console.log(inputQuantidade.value);
     }
 }
